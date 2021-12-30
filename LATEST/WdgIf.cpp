c4fa89f583fac4045f2fc6fe68b4d3969f31ec4d
root <nagaraja.hm.0001@gmail.com>
2021-12-30 21:50:18 +0100

    Update: submodule WdgIf

---
+++ LATEST/WdgIf.cpp
@@ -0,0 +1,7 @@
+#include "WdgIf.h"
+
+class_WdgIf WdgIf;
+
+void class_WdgIf::InitFunction(void){
+}
+
---
+++ LATEST/WdgIf.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class class_WdgIf{
+   public:
+      void InitFunction(void);
+};
+
+extern class_WdgIf WdgIf;
+
