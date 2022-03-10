9c5207f5d7fae6af2f752b16a106a1568d448a60
root <nagaraja.hm.0001@gmail.com>
2022-03-10 12:48:34 +0100

    Update: AUTOSAR basic software ECU abstraction module WdgIf

--- LATEST/WdgIf.cpp
+++ LATEST/WdgIf.cpp
@@ -7,8 +7,8 @@
 /* #INCLUDES                                         */
 /*****************************************************/
 #include "module.h"
-#include "WdgIf_EcuM.h"
-#include "WdgIf_SchM.h"
+#include "infWdgIf_EcuM.h"
+#include "infWdgIf_SchM.h"
 #include "WdgIf_Unused.h"
 
 /*****************************************************/
@@ -28,6 +28,7 @@ class module_WdgIf:
    public:
       FUNC(void, WDGIF_CODE) InitFunction   (void);
       FUNC(void, WDGIF_CODE) DeInitFunction (void);
+      FUNC(void, WDGIF_CODE) GetVersionInfo (void);
       FUNC(void, WDGIF_CODE) MainFunction   (void);
 };
 
@@ -44,6 +45,7 @@ class module_WdgIf:
 /*****************************************************/
 module_WdgIf   WdgIf;
 infEcuMClient* gptrinfEcuMClient_WdgIf = &WdgIf;
+infDcmClient*  gptrinfDcmClient_WdgIf  = &WdgIf;
 infSchMClient* gptrinfSchMClient_WdgIf = &WdgIf;
 
 /*****************************************************/
@@ -53,6 +55,9 @@ FUNC(void, WDGIF_CODE) module_WdgIf::InitFunction(void){
 }
 
 FUNC(void, WDGIF_CODE) module_WdgIf::DeInitFunction(void){
+}
+
+FUNC(void, WDGIF_CODE) module_WdgIf::GetVersionInfo(void){
 }
 
 FUNC(void, WDGIF_CODE) module_WdgIf::MainFunction(void){
