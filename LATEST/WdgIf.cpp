56148efe873afb69e845338271481d5012410480
root <nagaraja.hm.0001@gmail.com>
2022-03-29 13:47:20 +0200

    Update: AUTOSAR basic software ECU abstraction module WdgIf

--- LATEST/WdgIf.cpp
+++ LATEST/WdgIf.cpp
@@ -7,7 +7,6 @@
 /* #INCLUDES                                                                  */
 /******************************************************************************/
 #include "module.hpp"
-#include "CfgWdgIf.hpp"
 #include "infWdgIf_EcuM.hpp"
 #include "infWdgIf_Dcm.hpp"
 #include "infWdgIf_SchM.hpp"
@@ -36,37 +35,40 @@ class module_WdgIf:
       public abstract_module
 {
    public:
+      module_WdgIf(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
+      }
       FUNC(void, WDGIF_CODE) InitFunction   (void);
       FUNC(void, WDGIF_CODE) DeInitFunction (void);
-      FUNC(void, WDGIF_CODE) GetVersionInfo (void);
       FUNC(void, WDGIF_CODE) MainFunction   (void);
-
-   private:
-      CONST(Std_TypeVersionInfo, WDGIF_CONST) VersionInfo = {
-            0x0000
-         ,  0xFFFF
-         ,  0x01
-         ,  '0'
-         ,  '1'
-         ,  '0'
-      };
 };
+
+extern VAR(module_WdgIf, WDGIF_VAR) WdgIf;
 
 /******************************************************************************/
 /* CONSTS                                                                     */
 /******************************************************************************/
+CONSTP2VAR(infEcuMClient, WDGIF_VAR, WDGIF_CONST) gptrinfEcuMClient_WdgIf = &WdgIf;
+CONSTP2VAR(infDcmClient,  WDGIF_VAR, WDGIF_CONST) gptrinfDcmClient_WdgIf  = &WdgIf;
+CONSTP2VAR(infSchMClient, WDGIF_VAR, WDGIF_CONST) gptrinfSchMClient_WdgIf = &WdgIf;
 
 /******************************************************************************/
 /* PARAMS                                                                     */
 /******************************************************************************/
+#include "CfgWdgIf.hpp"
 
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-VAR(module_WdgIf, WDGIF_VAR) WdgIf;
-CONSTP2VAR(infEcuMClient, WDGIF_VAR, WDGIF_CONST) gptrinfEcuMClient_WdgIf = &WdgIf;
-CONSTP2VAR(infDcmClient,  WDGIF_VAR, WDGIF_CONST) gptrinfDcmClient_WdgIf  = &WdgIf;
-CONSTP2VAR(infSchMClient, WDGIF_VAR, WDGIF_CONST) gptrinfSchMClient_WdgIf = &WdgIf;
+VAR(module_WdgIf, WDGIF_VAR) WdgIf(
+   {
+         0x0000
+      ,  0xFFFF
+      ,  0x01
+      ,  '0'
+      ,  '1'
+      ,  '0'
+   }
+);
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
@@ -77,14 +79,6 @@ FUNC(void, WDGIF_CODE) module_WdgIf::InitFunction(void){
 
 FUNC(void, WDGIF_CODE) module_WdgIf::DeInitFunction(void){
    WdgIf.IsInitDone = E_NOT_OK;
-}
-
-FUNC(void, WDGIF_CODE) module_WdgIf::GetVersionInfo(void){
-#if(STD_ON == WdgIf_DevErrorDetect)
-//TBD: API parameter check
-   Det_ReportError(
-   );
-#endif
 }
 
 FUNC(void, WDGIF_CODE) module_WdgIf::MainFunction(void){
