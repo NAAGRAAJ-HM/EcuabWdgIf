e4a9f5f9dc4900517e3da47aad98caff0599f563
root <nagaraja.hm.0001@gmail.com>
2022-04-06 12:59:08 +0200

    Update: AUTOSAR basic software ECU abstraction module WdgIf

--- LATEST/WdgIf.cpp
+++ LATEST/WdgIf.cpp
@@ -14,18 +14,18 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define WDGIF_AR_RELEASE_MAJOR_VERSION                                         4
-#define WDGIF_AR_RELEASE_MINOR_VERSION                                         3
+#define WDGIF_AR_RELEASE_VERSION_MAJOR                                         4
+#define WDGIF_AR_RELEASE_VERSION_MINOR                                         3
 
 /******************************************************************************/
 /* MACROS                                                                     */
 /******************************************************************************/
-#if(WDGIF_AR_RELEASE_MAJOR_VERSION != STD_AR_RELEASE_MAJOR_VERSION)
-   #error "Incompatible WDGIF_AR_RELEASE_MAJOR_VERSION!"
+#if(WDGIF_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible WDGIF_AR_RELEASE_VERSION_MAJOR!"
 #endif
 
-#if(WDGIF_AR_RELEASE_MINOR_VERSION != STD_AR_RELEASE_MINOR_VERSION)
-   #error "Incompatible WDGIF_AR_RELEASE_MINOR_VERSION!"
+#if(WDGIF_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible WDGIF_AR_RELEASE_VERSION_MINOR!"
 #endif
 
 /******************************************************************************/
@@ -63,8 +63,10 @@ CONSTP2VAR(infSchMClient, WDGIF_VAR, WDGIF_CONST) gptrinfSchMClient_WdgIf = &Wdg
 /******************************************************************************/
 VAR(module_WdgIf, WDGIF_VAR) WdgIf(
    {
-         0x0000
-      ,  0xFFFF
+         WDGIF_AR_RELEASE_VERSION_MAJOR
+      ,  WDGIF_AR_RELEASE_VERSION_MINOR
+      ,  0x00
+      ,  0xFF
       ,  0x01
       ,  '0'
       ,  '1'
