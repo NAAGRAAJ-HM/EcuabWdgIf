a2f97eb34a8b7796ea8b69a448fdfdfefed070da
root <nagaraja.hm.0001@gmail.com>
2022-04-07 09:49:49 +0200

    Update: AUTOSAR basic software ECU abstraction module WdgIf

--- LATEST/WdgIf.cpp
+++ LATEST/WdgIf.cpp
@@ -96,8 +96,14 @@ FUNC(void, WDGIF_CODE) module_WdgIf::InitFunction(
 #endif
       }
       else{
+         if(STD_LOW){
 // check lptrCfgModule for memory faults
-// use PBcfg_WdgIf as back-up configuration
+            lptrCfg = lptrCfgModule;
+         }
+         else{
+// use PBcfg_CanIf as back-up configuration
+            lptrCfg = PBcfg_CanIf;
+         }
       }
       IsInitDone = E_OK;
 #if(STD_ON == WdgIf_InitCheck)
