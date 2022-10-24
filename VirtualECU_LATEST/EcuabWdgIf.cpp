92ef9669a52a99ddf35490d423c1a66b57e8aa39
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:04:17 +0200

    Update: AUTOSAR basic software ECU abstraction module for watchdog stack - interface layer

--- VirtualECU_LATEST/EcuabWdgIf.cpp
+++ VirtualECU_LATEST/EcuabWdgIf.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : EcuabWdgIf.cpp                                                         */
+/* File   : EcuabWdgIf.cpp                                                    */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define ECUABWDGIF_AR_RELEASE_VERSION_MAJOR                                         4
-#define ECUABWDGIF_AR_RELEASE_VERSION_MINOR                                         3
+#define ECUABWDGIF_AR_RELEASE_VERSION_MAJOR                                    4
+#define ECUABWDGIF_AR_RELEASE_VERSION_MINOR                                    3
 
 /******************************************************************************/
 /* MACROS                                                                     */
