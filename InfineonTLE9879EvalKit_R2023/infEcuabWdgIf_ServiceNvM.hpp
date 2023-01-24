1760ac790317db425ca37efac11252d1425038ca
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:51:25 +0100

    Update: AUTOSAR basic software ECU abstraction module for watchdog stack - interface layer

---
+++ InfineonTLE9879EvalKit_R2023/infEcuabWdgIf_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infEcuabWdgIf_ServiceNvM.hpp                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTECUABWDGIF_AR_RELEASE_VERSION_MAJOR                               4
+#define CONSTECUABWDGIF_AR_RELEASE_VERSION_MINOR                               3
+
+#define Const_DefaultInit_ConstEcuabWdgIf                                                                              \
+   ,  {CONSTECUABWDGIF_AR_RELEASE_VERSION_MAJOR, CONSTECUABWDGIF_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
