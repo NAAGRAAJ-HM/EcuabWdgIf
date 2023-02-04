276c076e1d33909bd435b4a17aefa0e4b37c8503
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:30:30 +0100

    Update: AUTOSAR basic software ECU abstraction module for watchdog stack - interface layer

--- InfineonTLE9879EvalKit_R2023/infEcuabWdgIf_ServiceNvM_Types.hpp
+++ InfineonTLE9879EvalKit_R2023/infEcuabWdgIf_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_EcuabWdgIf_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_EcuabWdgIf_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
